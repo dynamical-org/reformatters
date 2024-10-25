779db1db803121733bdbebfd70bf47f23b3c9af0
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2024-10-25 12:12:28 -0400

    Use kubernetes secrets for creds in prod

--- noaa/gefs/forecast/reformat.py
+++ noaa/gefs/forecast/reformat.py
@@ -88,7 +88,6 @@ def reformat_kubernetes(
             "EPHEMERAL_STORAGE": "200Gi",
             "AWS_ACCESS_KEY_ID": os.environ["AWS_ACCESS_KEY_ID"],
             "AWS_SECRET_ACCESS_KEY": os.environ["AWS_SECRET_ACCESS_KEY"],
-            "AWS_SESSION_TOKEN": os.environ["AWS_SESSION_TOKEN"],
         },
     )
 
