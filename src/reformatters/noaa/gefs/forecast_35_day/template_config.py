8c4005b49c6dcce48b2ae4b650f00459452afd8b
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2025-02-28 23:40:12 -0500

    v0.2.0 - 2020-10-01 to present (#57)

--- src/reformatters/noaa/gefs/forecast_35_day/template_config.py
+++ src/reformatters/noaa/gefs/forecast_35_day/template_config.py
@@ -18,9 +18,9 @@ from reformatters.common.types import DatetimeLike
 from reformatters.noaa.gefs.gefs_config_models import GEFSDataVar, GEFSInternalAttrs
 
 DATASET_ID = "noaa-gefs-forecast-35-day"
-DATASET_VERSION = "0.1.0"
+DATASET_VERSION = "0.2.0"
 
-INIT_TIME_START = pd.Timestamp("2025-02-01T00:00")
+INIT_TIME_START = pd.Timestamp("2020-10-01T00:00")
 INIT_TIME_FREQUENCY = pd.Timedelta("24h")
 
 DATASET_ATTRIBUTES = DatasetAttributes(
