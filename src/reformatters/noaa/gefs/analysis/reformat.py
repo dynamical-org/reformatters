9a8076fc5d91061033677070bd76f5de25e7e0c9
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2025-03-19 13:02:41 -0400

    Comment out noaa-gefs-analysis operational update code, it's not updated yet

--- src/reformatters/noaa/gefs/analysis/reformat.py
+++ src/reformatters/noaa/gefs/analysis/reformat.py
@@ -1,31 +1,35 @@
-import concurrent.futures
+import concurrent.futures  # noqa: F401  # all these imports are used in the commented operational update code
 import json
-import os
+import os  # noqa: F401
 import subprocess
-from collections.abc import Iterable
-from concurrent.futures import ThreadPoolExecutor
-from itertools import product, starmap
+from collections.abc import Iterable  # noqa: F401
+from concurrent.futures import ThreadPoolExecutor  # noqa: F401
+from itertools import product, starmap  # noqa: F401
 
 import numpy as np
 import pandas as pd
-import sentry_sdk
+import sentry_sdk  # noqa: F401
 import xarray as xr
 import zarr
 
-from reformatters.common import docker, validation
+from reformatters.common import docker, validation  # noqa: F401
 from reformatters.common.config import Config  # noqa:F401
 from reformatters.common.iterating import (
     consume,
     dimension_slices,
     get_worker_jobs,
 )
-from reformatters.common.kubernetes import Job, ReformatCronJob, ValidationCronJob
-from reformatters.common.logging import get_logger
-from reformatters.common.types import Array1D, DatetimeLike
+from reformatters.common.kubernetes import (  # noqa: F401
+    Job,
+    ReformatCronJob,
+    ValidationCronJob,
+)
+from reformatters.common.logging import get_logger  # noqa: F401
+from reformatters.common.types import Array1D, DatetimeLike  # noqa: F401
 from reformatters.common.zarr import (
-    copy_data_var,
-    copy_zarr_metadata,
-    get_local_tmp_store,
+    copy_data_var,  # noqa: F401
+    copy_zarr_metadata,  # noqa: F401
+    get_local_tmp_store,  # noqa: F401
     get_mode,
     get_zarr_store,
 )
@@ -122,189 +126,189 @@ def reformat_chunks(
     )
 
 
-@sentry_sdk.monitor(
-    monitor_slug=f"{template.DATASET_ID}-reformat-operational-update",
-    monitor_config={
-        "schedule": {"type": "crontab", "value": _OPERATIONAL_CRON_SCHEDULE},
-        "timezone": "UTC",
-    },
-)
-def reformat_operational_update() -> None:
-    final_store = get_store()
-    tmp_store = get_local_tmp_store()
-    # Get the dataset, check what data is already present
-    ds = xr.open_zarr(final_store, decode_timedelta=True)
-    for coord in ds.coords.values():
-        coord.load()
-    last_existing_init_time = ds.init_time.max()
-    time_end = _get_operational_update_time_end()
-    template_ds = template.get_template(time_end)
-    template_ds.ingested_forecast_length.loc[{"init_time": ds.init_time.values}] = (
-        ds.ingested_forecast_length
-    )
+# @sentry_sdk.monitor(
+#     monitor_slug=f"{template.DATASET_ID}-reformat-operational-update",
+#     monitor_config={
+#         "schedule": {"type": "crontab", "value": _OPERATIONAL_CRON_SCHEDULE},
+#         "timezone": "UTC",
+#     },
+# )
+# def reformat_operational_update() -> None:
+#     final_store = get_store()
+#     tmp_store = get_local_tmp_store()
+#     # Get the dataset, check what data is already present
+#     ds = xr.open_zarr(final_store, decode_timedelta=True)
+#     for coord in ds.coords.values():
+#         coord.load()
+#     last_existing_init_time = ds.init_time.max()
+#     time_end = _get_operational_update_time_end()
+#     template_ds = template.get_template(time_end)
+#     template_ds.ingested_forecast_length.loc[{"init_time": ds.init_time.values}] = (
+#         ds.ingested_forecast_length
+#     )
 
-    # Uncomment this line for local testing to scope down the number of init times
-    # template_ds = template_ds.isel(init_time=slice(0, len(ds.init_time) + 2))
+#     # Uncomment this line for local testing to scope down the number of init times
+#     # template_ds = template_ds.isel(init_time=slice(0, len(ds.init_time) + 2))
 
-    # We make some assumptions about what is safe to parallelize and how to
-    # write the data based on the init_time dimension having a shard size of one.
-    # If this changes we will need to refactor.
-    assert all(
-        1 == da.encoding["shards"][da.dims.index(template.APPEND_DIMENSION)]
-        for da in ds.data_vars.values()
-    )
-    new_init_times = template_ds.init_time.loc[
-        template_ds.init_time > last_existing_init_time
-    ]
-    new_init_time_indices = template_ds.get_index(
-        template.APPEND_DIMENSION
-    ).get_indexer(new_init_times)  # type: ignore
-    new_init_time_i_slices = list(
-        starmap(
-            slice,
-            zip(
-                new_init_time_indices,
-                list(new_init_time_indices[1:]) + [None],
-                strict=False,
-            ),
-        )
-    )
-    # In addition to new dates, reprocess old dates whose forecasts weren't
-    # previously fully written.
-    # On long forecasts, NOAA may fill in the first 10 days first, then publish
-    # an additional 25 days after a delay.
-    recent_incomplete_init_times = _get_recent_init_times_for_reprocessing(ds)
-    recent_incomplete_init_times_indices = template_ds.get_index(
-        template.APPEND_DIMENSION
-    ).get_indexer(recent_incomplete_init_times)  # type: ignore
-    recent_incomplete_init_time_i_slices = list(
-        starmap(
-            slice,
-            zip(
-                recent_incomplete_init_times_indices,
-                [x + 1 for x in recent_incomplete_init_times_indices],
-                strict=False,
-            ),
-        )
-    )
-    upload_executor = ThreadPoolExecutor(max_workers=(os.cpu_count() or 1) * 2)
+#     # We make some assumptions about what is safe to parallelize and how to
+#     # write the data based on the init_time dimension having a shard size of one.
+#     # If this changes we will need to refactor.
+#     assert all(
+#         1 == da.encoding["shards"][da.dims.index(template.APPEND_DIMENSION)]
+#         for da in ds.data_vars.values()
+#     )
+#     new_init_times = template_ds.init_time.loc[
+#         template_ds.init_time > last_existing_init_time
+#     ]
+#     new_init_time_indices = template_ds.get_index(
+#         template.APPEND_DIMENSION
+#     ).get_indexer(new_init_times)  # type: ignore
+#     new_init_time_i_slices = list(
+#         starmap(
+#             slice,
+#             zip(
+#                 new_init_time_indices,
+#                 list(new_init_time_indices[1:]) + [None],
+#                 strict=False,
+#             ),
+#         )
+#     )
+#     # In addition to new dates, reprocess old dates whose forecasts weren't
+#     # previously fully written.
+#     # On long forecasts, NOAA may fill in the first 10 days first, then publish
+#     # an additional 25 days after a delay.
+#     recent_incomplete_init_times = _get_recent_init_times_for_reprocessing(ds)
+#     recent_incomplete_init_times_indices = template_ds.get_index(
+#         template.APPEND_DIMENSION
+#     ).get_indexer(recent_incomplete_init_times)  # type: ignore
+#     recent_incomplete_init_time_i_slices = list(
+#         starmap(
+#             slice,
+#             zip(
+#                 recent_incomplete_init_times_indices,
+#                 [x + 1 for x in recent_incomplete_init_times_indices],
+#                 strict=False,
+#             ),
+#         )
+#     )
+#     upload_executor = ThreadPoolExecutor(max_workers=(os.cpu_count() or 1) * 2)
 
-    for init_time_i_slice in (
-        recent_incomplete_init_time_i_slices + new_init_time_i_slices
-    ):
-        truncated_template_ds = template_ds.isel(
-            init_time=slice(0, init_time_i_slice.stop)
-        )
-        # Write through this i_slice. Metadata is required for to_zarr's region="auto" option.
-        template.write_metadata(
-            truncated_template_ds,
-            tmp_store,
-            get_mode(tmp_store),
-        )
-        data_var_upload_futures = []
-        for data_var, max_lead_times in reformat_time_i_slices(
-            [(init_time_i_slice, list(template_ds.data_vars.keys()))],
-            template_ds,
-            tmp_store,
-            _VARIABLES_PER_BACKFILL_JOB,
-        ):
-            # Writing a chunk without merging in existing data works because
-            # the init_time dimension chunk size is 1.
-            data_var_upload_futures.append(
-                upload_executor.submit(
-                    copy_data_var,
-                    data_var.name,
-                    init_time_i_slice.start,
-                    tmp_store,
-                    final_store,
-                )
-            )
-            for (init_time, ensemble_member), max_lead_time in max_lead_times.items():
-                if np.issubdtype(type(ensemble_member), np.integer):
-                    truncated_template_ds["ingested_forecast_length"].loc[
-                        {"init_time": init_time, "ensemble_member": ensemble_member}
-                    ] = max_lead_time
+#     for init_time_i_slice in (
+#         recent_incomplete_init_time_i_slices + new_init_time_i_slices
+#     ):
+#         truncated_template_ds = template_ds.isel(
+#             init_time=slice(0, init_time_i_slice.stop)
+#         )
+#         # Write through this i_slice. Metadata is required for to_zarr's region="auto" option.
+#         template.write_metadata(
+#             truncated_template_ds,
+#             tmp_store,
+#             get_mode(tmp_store),
+#         )
+#         data_var_upload_futures = []
+#         for data_var, max_lead_times in reformat_time_i_slices(
+#             [(init_time_i_slice, list(template_ds.data_vars.keys()))],
+#             template_ds,
+#             tmp_store,
+#             _VARIABLES_PER_BACKFILL_JOB,
+#         ):
+#             # Writing a chunk without merging in existing data works because
+#             # the init_time dimension chunk size is 1.
+#             data_var_upload_futures.append(
+#                 upload_executor.submit(
+#                     copy_data_var,
+#                     data_var.name,
+#                     init_time_i_slice.start,
+#                     tmp_store,
+#                     final_store,
+#                 )
+#             )
+#             for (init_time, ensemble_member), max_lead_time in max_lead_times.items():
+#                 if np.issubdtype(type(ensemble_member), np.integer):
+#                     truncated_template_ds["ingested_forecast_length"].loc[
+#                         {"init_time": init_time, "ensemble_member": ensemble_member}
+#                     ] = max_lead_time
 
-        concurrent.futures.wait(data_var_upload_futures, return_when="FIRST_EXCEPTION")
-        for future in data_var_upload_futures:
-            if (e := future.exception()) is not None:
-                raise e
+#         concurrent.futures.wait(data_var_upload_futures, return_when="FIRST_EXCEPTION")
+#         for future in data_var_upload_futures:
+#             if (e := future.exception()) is not None:
+#                 raise e
 
-        # Write metadata again to update the ingested_forecast_length
-        template.write_metadata(
-            truncated_template_ds,
-            tmp_store,
-            get_mode(tmp_store),
-        )
-        # Write the metadata last, the data must be written first
-        copy_zarr_metadata(truncated_template_ds, tmp_store, final_store)
-
-
-def _get_operational_update_time_end() -> pd.Timestamp:
-    return pd.Timestamp.utcnow().tz_localize(None)
+#         # Write metadata again to update the ingested_forecast_length
+#         template.write_metadata(
+#             truncated_template_ds,
+#             tmp_store,
+#             get_mode(tmp_store),
+#         )
+#         # Write the metadata last, the data must be written first
+#         copy_zarr_metadata(truncated_template_ds, tmp_store, final_store)
 
 
-def _get_recent_init_times_for_reprocessing(ds: xr.Dataset) -> Array1D[np.datetime64]:
-    # Get the last few days of data
-    recent_init_times = ds.init_time.where(
-        ds.init_time > pd.Timestamp.utcnow().tz_localize(None) - np.timedelta64(4, "D"),
-        drop=True,
-    ).load()
-    # Ingested forecast length is along init time and ensemble member.
-    # We care if any of the ensemble members for this init time
-    # are not fully ingested.
-    recent_init_times["reduced_ingested_forecast_length"] = (
-        ds.ingested_forecast_length.min(dim="ensemble_member")
-    )
-    # Get the recent init_times where we have only partially completed
-    # the ingest.
-    return recent_init_times.where(  # type: ignore
-        (recent_init_times.reduced_ingested_forecast_length.isnull())
-        | (
-            recent_init_times.reduced_ingested_forecast_length
-            < recent_init_times.expected_forecast_length
-        ),
-        drop=True,
-    ).init_time.values
+# def _get_operational_update_time_end() -> pd.Timestamp:
+#     return pd.Timestamp.utcnow().tz_localize(None)
 
 
-@sentry_sdk.monitor(
-    monitor_slug=f"{template.DATASET_ID}-validation",
-    monitor_config={
-        "schedule": {"type": "crontab", "value": _VALIDATION_CRON_SCHEDULE},
-        "timezone": "UTC",
-    },
-)
-def validate_zarr() -> None:
-    validation.validate_zarr(
-        get_store(),
-        validators=(
-            validation.check_forecast_current_data,
-            validation.check_forecast_recent_nans,
-        ),
-    )
+# def _get_recent_init_times_for_reprocessing(ds: xr.Dataset) -> Array1D[np.datetime64]:
+#     # Get the last few days of data
+#     recent_init_times = ds.init_time.where(
+#         ds.init_time > pd.Timestamp.utcnow().tz_localize(None) - np.timedelta64(4, "D"),
+#         drop=True,
+#     ).load()
+#     # Ingested forecast length is along init time and ensemble member.
+#     # We care if any of the ensemble members for this init time
+#     # are not fully ingested.
+#     recent_init_times["reduced_ingested_forecast_length"] = (
+#         ds.ingested_forecast_length.min(dim="ensemble_member")
+#     )
+#     # Get the recent init_times where we have only partially completed
+#     # the ingest.
+#     return recent_init_times.where(  # type: ignore
+#         (recent_init_times.reduced_ingested_forecast_length.isnull())
+#         | (
+#             recent_init_times.reduced_ingested_forecast_length
+#             < recent_init_times.expected_forecast_length
+#         ),
+#         drop=True,
+#     ).init_time.values
 
 
-def operational_kubernetes_resources(image_tag: str) -> Iterable[Job]:
-    operational_update_cron_job = ReformatCronJob(
-        name=f"{template.DATASET_ID}-operational-update",
-        schedule=_OPERATIONAL_CRON_SCHEDULE,
-        image=image_tag,
-        dataset_id=template.DATASET_ID,
-        cpu="6",  # fit on 8 vCPU node
-        memory="60G",  # fit on 64GB node
-        ephemeral_storage="150G",
-    )
-    validation_cron_job = ValidationCronJob(
-        name=f"{template.DATASET_ID}-validation",
-        schedule=_VALIDATION_CRON_SCHEDULE,
-        image=image_tag,
-        dataset_id=template.DATASET_ID,
-        cpu="3",  # fit on 4 vCPU node
-        memory="30G",  # fit on 32GB node
-    )
+# @sentry_sdk.monitor(
+#     monitor_slug=f"{template.DATASET_ID}-validation",
+#     monitor_config={
+#         "schedule": {"type": "crontab", "value": _VALIDATION_CRON_SCHEDULE},
+#         "timezone": "UTC",
+#     },
+# )
+# def validate_zarr() -> None:
+#     validation.validate_zarr(
+#         get_store(),
+#         validators=(
+#             validation.check_forecast_current_data,
+#             validation.check_forecast_recent_nans,
+#         ),
+#     )
 
-    return [operational_update_cron_job, validation_cron_job]
+
+# def operational_kubernetes_resources(image_tag: str) -> Iterable[Job]:
+#     operational_update_cron_job = ReformatCronJob(
+#         name=f"{template.DATASET_ID}-operational-update",
+#         schedule=_OPERATIONAL_CRON_SCHEDULE,
+#         image=image_tag,
+#         dataset_id=template.DATASET_ID,
+#         cpu="6",  # fit on 8 vCPU node
+#         memory="60G",  # fit on 64GB node
+#         ephemeral_storage="150G",
+#     )
+#     validation_cron_job = ValidationCronJob(
+#         name=f"{template.DATASET_ID}-validation",
+#         schedule=_VALIDATION_CRON_SCHEDULE,
+#         image=image_tag,
+#         dataset_id=template.DATASET_ID,
+#         cpu="3",  # fit on 4 vCPU node
+#         memory="30G",  # fit on 32GB node
+#     )
+
+#     return [operational_update_cron_job, validation_cron_job]
 
 
 def all_jobs_ordered(
