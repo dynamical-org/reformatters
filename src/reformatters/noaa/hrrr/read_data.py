50c6d36c0986d35d18ed7eb64d092081b6e23014
Tony Cannistra <tony.cannistra@gmail.com>
2025-03-21 08:10:42 -0700

    remove print

--- src/reformatters/noaa/hrrr/read_data.py
+++ src/reformatters/noaa/hrrr/read_data.py
@@ -36,8 +36,6 @@ def download_file(
     init_date_str = init_time.strftime("%Y%m%d")
     init_hour_str = init_time.strftime("%H")
 
-    print(lead_time_hours)
-
     remote_path = f"hrrr.{init_date_str}/{domain}/hrrr.t{init_hour_str}z.wrf{hrrr_file_type}f{int(lead_time_hours):02d}.grib2"
 
     store = http_store("https://noaa-hrrr-bdp-pds.s3.amazonaws.com")
