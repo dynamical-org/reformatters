dcacd0b97d7b9158e1009791fba27144e0d0fc4c
Tony Cannistra <tony.cannistra@gmail.com>
2025-03-21 01:55:39 +0000

    new models

--- src/reformatters/noaa/hrrr/hrrr_config_models.py
+++ src/reformatters/noaa/hrrr/hrrr_config_models.py
@@ -1,9 +1,20 @@
+from typing import Literal
+
 from reformatters.common.config_models import DataVar
 from reformatters.noaa.noaa_config_models import NOAAInternalAttrs
 
+type HRRRDomain = Literal["alaska", "conus"]
+
+# HRRR Provides 4 datasets corresponding to sets of vertical levels
+# - prs: 3D pressure levels
+# - nat: Native levels
+# - sfc: 2D surface levels
+# - subh: 2D surface levels, sub-hourly
+type HRRRFileType = Literal["prs", "nat", "sfc", "subh"]
+
 
 class HRRRInternalAttrs(NOAAInternalAttrs):
-    pass
+    hrrr_file_type: HRRRFileType
 
 
 class HRRRDataVar(DataVar[HRRRInternalAttrs]):
