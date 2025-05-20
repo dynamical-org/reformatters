4034676f3cf259164727eb4fe3721a2ca4711bc7
Alex Gontar <alex@upstream.tech>
2025-05-20 11:10:44 -0400

    Support env variable log level

--- src/reformatters/common/logging.py
+++ src/reformatters/common/logging.py
@@ -1,11 +1,12 @@
 import logging
+import os
 import time
 
 # Configure logging to include UTC date and time
 logging.basicConfig(
     format=f"%(asctime)s {logging.BASIC_FORMAT}",
     datefmt="%Y-%m-%dT%H:%M:%SZ",
-    level=logging.INFO,
+    level=os.environ.get("LOG_LEVEL", logging.INFO),
 )
 # Ensure timestamps are in UTC
 logging.Formatter.converter = time.gmtime
