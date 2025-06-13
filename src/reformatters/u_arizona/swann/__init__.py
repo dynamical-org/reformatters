7bae8caadd9d4bbdd65f229de2ef3fefca85520e
Alex <mosegontar@users.noreply.github.com>
2025-06-13 10:48:26 -0400

    Add SWANN validators (#146)
    * Add SWANN validators and update common function
    
    * Revert the change to common validators
    
    * SWANN validators
    
    * Move tests to swann validators
    
    * Fix check_random_time_within_last_year_nans

--- src/reformatters/u_arizona/swann/__init__.py
+++ src/reformatters/u_arizona/swann/__init__.py
@@ -1,9 +1,24 @@
+from collections.abc import Sequence
+
+from reformatters.common import validation
 from reformatters.common.dynamical_dataset import DynamicalDataset
 
 from .region_job import SWANNRegionJob, SWANNSourceFileCoord
 from .template_config import SWANNDataVar, SWANNTemplateConfig
+from .validators import (
+    check_data_is_current,
+    check_latest_time_nans,
+    check_random_time_within_last_year_nans,
+)
 
 
 class SWANNDataset(DynamicalDataset[SWANNDataVar, SWANNSourceFileCoord]):
     template_config: SWANNTemplateConfig = SWANNTemplateConfig()
     region_job_class: type[SWANNRegionJob] = SWANNRegionJob
+
+    def validators(self) -> Sequence[validation.DataValidator]:
+        return (
+            check_data_is_current,
+            check_latest_time_nans,
+            check_random_time_within_last_year_nans,
+        )
