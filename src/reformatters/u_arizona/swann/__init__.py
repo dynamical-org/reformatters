760f14235641a6a169f47bbea7ee2023d5352479
Sam Neubardt <sam@upstream.tech>
2025-06-16 12:43:59 -0400

    Don't deploy SWANN yet since we need a new storage config

--- src/reformatters/u_arizona/swann/__init__.py
+++ src/reformatters/u_arizona/swann/__init__.py
@@ -1,9 +1,7 @@
-from collections.abc import Iterable, Sequence
-from datetime import timedelta
+from collections.abc import Sequence
 
 from reformatters.common import validation
 from reformatters.common.dynamical_dataset import DynamicalDataset
-from reformatters.common.kubernetes import Job, ReformatCronJob, ValidationCronJob
 
 from .region_job import SWANNRegionJob, SWANNSourceFileCoord
 from .template_config import SWANNDataVar, SWANNTemplateConfig
@@ -28,28 +26,28 @@ class SWANNDataset(DynamicalDataset[SWANNDataVar, SWANNSourceFileCoord]):
             check_random_time_within_last_year_nans,
         )
 
-    def operational_kubernetes_resources(self, image_tag: str) -> Iterable[Job]:
-        operational_update_cron_job = ReformatCronJob(
-            name=f"{self.dataset_id}-operational-update",
-            schedule=_OPERATIONAL_CRON_SCHEDULE,
-            pod_active_deadline=timedelta(minutes=30),
-            image=image_tag,
-            dataset_id=self.dataset_id,
-            cpu="4",
-            memory="14G",
-            shared_memory="6GiB",
-            ephemeral_storage="10G",
-            secret_names=[self.storage_config.k8s_secret_name],
-        )
-        validation_cron_job = ValidationCronJob(
-            name=f"{self.dataset_id}-validation",
-            schedule=_VALIDATION_CRON_SCHEDULE,
-            pod_active_deadline=timedelta(minutes=10),
-            image=image_tag,
-            dataset_id=self.dataset_id,
-            cpu="1.3",
-            memory="7G",
-            secret_names=[self.storage_config.k8s_secret_name],
-        )
+    # def operational_kubernetes_resources(self, image_tag: str) -> Iterable[Job]:
+    #     operational_update_cron_job = ReformatCronJob(
+    #         name=f"{self.dataset_id}-operational-update",
+    #         schedule=_OPERATIONAL_CRON_SCHEDULE,
+    #         pod_active_deadline=timedelta(minutes=30),
+    #         image=image_tag,
+    #         dataset_id=self.dataset_id,
+    #         cpu="4",
+    #         memory="14G",
+    #         shared_memory="6GiB",
+    #         ephemeral_storage="10G",
+    #         secret_names=[self.storage_config.k8s_secret_name],
+    #     )
+    #     validation_cron_job = ValidationCronJob(
+    #         name=f"{self.dataset_id}-validation",
+    #         schedule=_VALIDATION_CRON_SCHEDULE,
+    #         pod_active_deadline=timedelta(minutes=10),
+    #         image=image_tag,
+    #         dataset_id=self.dataset_id,
+    #         cpu="1.3",
+    #         memory="7G",
+    #         secret_names=[self.storage_config.k8s_secret_name],
+    #     )
 
-        return [operational_update_cron_job, validation_cron_job]
+    #     return [operational_update_cron_job, validation_cron_job]
