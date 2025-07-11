121366990e2e67b067c1f1e6044ab689686efe7e
Alex <mosegontar@users.noreply.github.com>
2025-07-11 18:52:01 +0000

    Add boto3 to support lsing S3 buckets anonymously (#178)
    * Add boto3 to support lsing bucket anonymously
    
    * Add comment about boto3 version
    
    * Increase emphermal storage, comment out op resources, set download parallelism to 4

--- src/reformatters/contrib/noaa/ndvi_cdr/ndvi_cdr/analysis/dynamical_dataset.py
+++ src/reformatters/contrib/noaa/ndvi_cdr/ndvi_cdr/analysis/dynamical_dataset.py
@@ -1,9 +1,7 @@
 from collections.abc import Sequence
-from datetime import timedelta
 
 from reformatters.common import validation
 from reformatters.common.dynamical_dataset import DynamicalDataset
-from reformatters.common.kubernetes import CronJob, ReformatCronJob, ValidationCronJob
 
 from .region_job import NoaaNdviCdrAnalysisRegionJob, NoaaNdviCdrAnalysisSourceFileCoord
 from .template_config import NoaaNdviCdrAnalysisTemplateConfig, NoaaNdviCdrDataVar
@@ -18,32 +16,32 @@ class NoaaNdviCdrAnalysisDataset(
     )
     region_job_class: type[NoaaNdviCdrAnalysisRegionJob] = NoaaNdviCdrAnalysisRegionJob
 
-    def operational_kubernetes_resources(self, image_tag: str) -> Sequence[CronJob]:
-        """Return the kubernetes cron job definitions to operationally update and validate this dataset."""
-        operational_update_cron_job = ReformatCronJob(
-            name=f"{self.dataset_id}-operational-update",
-            schedule="0 20 * * *",
-            pod_active_deadline=timedelta(minutes=30),
-            image=image_tag,
-            dataset_id=self.dataset_id,
-            cpu="2",
-            memory="230G",
-            shared_memory="190Gi",
-            ephemeral_storage="30G",
-            secret_names=self.storage_config.k8s_secret_names,
-        )
-        validation_cron_job = ValidationCronJob(
-            name=f"{self.dataset_id}-validation",
-            schedule="30 20 * * *",
-            pod_active_deadline=timedelta(minutes=10),
-            image=image_tag,
-            dataset_id=self.dataset_id,
-            cpu="1.3",
-            memory="7G",
-            secret_names=self.storage_config.k8s_secret_names,
-        )
+    # def operational_kubernetes_resources(self, image_tag: str) -> Sequence[CronJob]:
+    #    """Return the kubernetes cron job definitions to operationally update and validate this dataset."""
+    #    operational_update_cron_job = ReformatCronJob(
+    #        name=f"{self.dataset_id}-operational-update",
+    #        schedule="0 20 * * *",
+    #        pod_active_deadline=timedelta(minutes=30),
+    #        image=image_tag,
+    #        dataset_id=self.dataset_id,
+    #        cpu="2",
+    #        memory="230G",
+    #        shared_memory="190Gi",
+    #        ephemeral_storage="150G",
+    #        secret_names=self.storage_config.k8s_secret_names,
+    #    )
+    #    validation_cron_job = ValidationCronJob(
+    #        name=f"{self.dataset_id}-validation",
+    #        schedule="30 20 * * *",
+    #        pod_active_deadline=timedelta(minutes=10),
+    #        image=image_tag,
+    #        dataset_id=self.dataset_id,
+    #        cpu="1.3",
+    #        memory="7G",
+    #        secret_names=self.storage_config.k8s_secret_names,
+    #    )
 
-        return [operational_update_cron_job, validation_cron_job]
+    #    return [operational_update_cron_job, validation_cron_job]
 
     def validators(self) -> Sequence[validation.DataValidator]:
         """Return a sequence of DataValidators to run on this dataset."""
