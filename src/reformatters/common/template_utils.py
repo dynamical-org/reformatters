845bc53f4e704ae6f48705aee2a0474a776e449f
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2025-08-06 23:44:30 -0400

    Replace prints with logs

--- src/reformatters/common/template_utils.py
+++ src/reformatters/common/template_utils.py
@@ -44,7 +44,6 @@ def write_metadata(
             message="Consolidated metadata is currently not part in the Zarr format 3 specification",
             category=UserWarning,
         )
-        print(template_ds)
         template_ds.to_zarr(store, mode=mode, compute=False)  # type: ignore[call-overload]
 
     if isinstance(store, Path | str):
