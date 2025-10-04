02cc590e72f17b59d13b4bd384ca357d20317a9c
Alden Keefe Sampson <alden@dynamical.org>
2025-10-03 23:41:55 -0400

    feat: add latitude/longitude coordinates to SMAP Level 3 template

--- tests/contrib/nasa/smap/level3_36km_v9/template_config_test.py
+++ tests/contrib/nasa/smap/level3_36km_v9/template_config_test.py
@@ -78,11 +78,11 @@ def test_latitude_longitude_coordinates() -> None:
     assert lat.shape == (406, 964)
     assert lon.shape == (406, 964)
 
-    assert np.isclose(np.min(lat), -85.044502, atol=0.01)
-    assert np.isclose(np.max(lat), 85.044502, atol=0.01)
+    assert np.isclose(np.min(lat), -83.6, atol=0.1)
+    assert np.isclose(np.max(lat), 83.6, atol=0.1)
 
-    assert np.isclose(np.min(lon), -180.0, atol=0.01)
-    assert np.isclose(np.max(lon), 180.0, atol=0.01)
+    assert np.isclose(np.min(lon), -179.8, atol=0.1)
+    assert np.isclose(np.max(lon), 179.8, atol=0.1)
 
     assert lat[0, 0] > lat[-1, 0]
 
