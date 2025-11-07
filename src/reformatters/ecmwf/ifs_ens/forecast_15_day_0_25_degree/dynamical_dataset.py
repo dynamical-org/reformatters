985e21513c7ac2543a9cc4f0bb37a521bc7c2d6b
Alex Gontar <alex@upstream.tech>
2025-11-07 11:45:11 -0500

    Update validation cron schedule

--- src/reformatters/ecmwf/ifs_ens/forecast_15_day_0_25_degree/dynamical_dataset.py
+++ src/reformatters/ecmwf/ifs_ens/forecast_15_day_0_25_degree/dynamical_dataset.py
@@ -45,7 +45,7 @@ class EcmwfIfsEnsForecast15Day025DegreeDataset(
         )
         validation_cron_job = ValidationCronJob(
             name=f"{self.dataset_id}-validation",
-            schedule="40 8 * * *",  # 50 minutes after update starts
+            schedule="50 10 * * *",  # 3 Hours after update starts (update may take ~2 hours)
             suspend=False,
             pod_active_deadline=timedelta(minutes=10),
             image=image_tag,
