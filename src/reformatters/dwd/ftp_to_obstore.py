329eb331145cd9565d4f10f5ae8953a67365c8e8
Jack Kelly <jack@openclimatefix.org>
2026-01-14 10:17:02 +0000

    Re-order code to make it more readable

--- src/reformatters/dwd/ftp_to_obstore.py
+++ src/reformatters/dwd/ftp_to_obstore.py
@@ -17,99 +17,7 @@ from reformatters.common.logging import get_logger
 log = get_logger(__name__)
 
 
-def _log_rclone_stderr(stderr: str) -> None:
-    for line in stderr.splitlines():
-        clean_line = line.strip()
-        if not clean_line:
-            continue
-
-        # rclone uses "Error", "Warning", "Info" in its output.
-        line_upper = clean_line.upper()
-        if "ERROR" in line_upper or "FAILED" in line_upper:
-            log_level = logging.ERROR
-        elif "WARNING" in line_upper:
-            log_level = logging.WARNING
-        else:
-            log_level = logging.INFO
-
-        log.log(level=log_level, msg=f"Rclone: {clean_line}")
-
-
-# Load the libc library to access prctl
-libc = ctypes.CDLL("libc.so.6")
-
-
-def _set_death_signal() -> None:
-    """Send SIGTERM to the child process if the parent process (python) dies."""
-    pr_set_pdeathsig = 1
-    libc.prctl(pr_set_pdeathsig, signal.SIGTERM)
-
-
-def _call_command_with_logging(cmd: list, timeout: int = 90) -> str:
-    log.info("Command: %s", " ".join(cmd))
-
-    # --------- Killing the subprocess when Python dies ---------------
-    # The default behaviour for `subprocess.Popen` is that `process` will continue running as a
-    # zombie process if python dies before `process` finishes. We don't want that! We can ensure
-    # `process` is killed if python is killed. But there are two ways Python could be killed:
-    #
-    # 1. `kill -9 python` (SIGKILL) or `kill python` (SIGTERM) -> the OS instantly kills Python.
-    #    Python doesn't have time to throw any exceptions. We set `preexec_fn` to handle this case.
-    # 2. `Ctrl+C` -> the OS sends SIGINT to Python -> Python raises a KeyboardInterrupt -> We catch
-    #     this and call `process.terminate()`. prctl DOES NOT fire here because Python is still "alive" handling the exception.
-
-    process_name = cmd[0]
-    process = subprocess.Popen(  # noqa: S603
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,  # rclone sends its progress and status messages to stderr.
-        text=True,  # Open stdout and stderr text mode (not bytes).
-        bufsize=1,  # Enable line buffering for stdout and stderr.
-        # preexec_fn will be called in the child process just before cmd is executed.
-        # WARNING: preexec_fn is not supported in threaded code or in Python subinterpreters.
-        preexec_fn=_set_death_signal,  # noqa: PLW1509
-    )
-    try:
-        # You might wonder why we're using `process.communicate` instead of `process.wait`.
-        # `process.wait` deadlocks if the process returns lots of data in a pipe. In our case,
-        # `process.wait` deadlocks when `rclone` returns a long directory listing through stdout.
-        stdout_str, stderr_str = process.communicate(timeout=timeout)
-    except (KeyboardInterrupt, SystemExit):
-        _terminate_child_process(process, process_name)
-        raise
-
-    _log_rclone_stderr(stderr_str)
-
-    if process.returncode == 0:
-        return stdout_str
-    else:
-        raise subprocess.CalledProcessError(
-            cmd=cmd, returncode=process.returncode, output=stdout_str, stderr=stderr_str
-        )
-
-
-def _terminate_child_process(process: subprocess.Popen, process_name: str) -> None:
-    log.exception("Terminating child process %s...", process_name)
-    process.terminate()
-    log.info(
-        "Waiting 5 seconds for child %s process to terminate gracefully...",
-        process_name,
-    )
-    try:
-        stdout_str, stderr_str = process.communicate(timeout=5)
-    except subprocess.TimeoutExpired:
-        log.warning(
-            "Child process %s has not terminated gracefully. So we will forcefully kill it.",
-            process_name,
-        )
-        process.kill()
-    else:
-        _log_rclone_stderr(stderr_str)
-        if stdout_str:
-            log.info("%s stdout during shutdown: %s", process_name, stdout_str)
-
-
-class _ListItem(TypedDict):
+class _FtpListItem(TypedDict):
     """An item returned by rclone's lsjson command."""
 
     Path: ReadOnly[str]
@@ -138,12 +46,12 @@ class FtpToObstore:
         self.ftp_path = ftp_path
         self.dst_path = dst_path
 
-    def ftp_list(self) -> list[_ListItem]:
+    def ftp_list(self) -> list[_FtpListItem]:
         """
         Recursively list all files and directories below ftp_host/ftp_path.
 
         Returns a list of ListItem dictionaries. Note that the `Path` attribute in the returned dict
-        does not include the `self.path`. For example, a returned `Path` might look like
+        does not include `self.path`. For example, a returned `Path` might look like
         "aswdifd_s/icon-eu_europe_regular-lat-lon_single-level_2026011200_004_ASWDIFD_S.grib2.bz2"
         """
         log.info(f"Listing ftp://{self.ftp_host}{self.ftp_path} ...")
@@ -155,7 +63,7 @@ class FtpToObstore:
             "--no-mimetype",  # Don't read the mime type (can speed things up).
             "--no-modtime",  # Don't read the modification time (can speed things up).
             f":ftp:{self.ftp_path}",
-            *self._ftp_host_and_user_and_pass,
+            *self._ftp_host_and_credentials,
             *self._common_params_for_rclone,
         ]
 
@@ -171,7 +79,7 @@ class FtpToObstore:
             raise
 
     @property
-    def _ftp_host_and_user_and_pass(self) -> list[str]:
+    def _ftp_host_and_credentials(self) -> list[str]:
         return [
             "--ftp-host=" + self.ftp_host,
             "--ftp-user=anonymous",
@@ -188,7 +96,7 @@ class FtpToObstore:
 
     def compute_directories_to_copy(
         self,
-        file_list: list[_ListItem],
+        file_list: list[_FtpListItem],
     ) -> dict[_SrcAndDstPath, list[str]]:
         """
         Returns a dict which maps from (src_directory, dst_directory) to a list of filenames.
@@ -283,7 +191,7 @@ class FtpToObstore:
                     "--transfers=10",  # Increase parallelism.
                     "--no-check-certificate",
                     "--no-traverse",
-                    *self._ftp_host_and_user_and_pass,
+                    *self._ftp_host_and_credentials,
                     *self._common_params_for_rclone,
                 ]
 
@@ -294,3 +202,92 @@ class FtpToObstore:
                 else:
                     if stdout_str:
                         log.debug("rclone stdout: %s", stdout_str)
+
+
+def _call_command_with_logging(cmd: list, timeout: int = 90) -> str:
+    log.info("Command: %s", " ".join(cmd))
+
+    # --------- Killing the subprocess when Python dies ---------------
+    # The default behaviour for `subprocess.Popen` is that `process` will continue running as a
+    # zombie process if python dies before `process` finishes. We don't want that! We can ensure
+    # `process` is killed if python is killed. But there are two ways Python could be killed:
+    #
+    # 1. `kill -9 python` (SIGKILL) or `kill python` (SIGTERM) -> the OS instantly kills Python.
+    #    Python doesn't have time to throw any exceptions. We set `preexec_fn` to handle this case.
+    # 2. `Ctrl+C` -> the OS sends SIGINT to Python -> Python raises a KeyboardInterrupt -> We catch
+    #     this and call `process.terminate()`. prctl DOES NOT fire here because Python is still "alive" handling the exception.
+
+    process_name = cmd[0]
+    process = subprocess.Popen(  # noqa: S603
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,  # rclone sends its progress and status messages to stderr.
+        text=True,  # Open stdout and stderr text mode (not bytes).
+        bufsize=1,  # Enable line buffering for stdout and stderr.
+        # preexec_fn will be called in the child process just before cmd is executed.
+        # WARNING: preexec_fn is not supported in threaded code or in Python subinterpreters.
+        preexec_fn=_set_death_signal,  # noqa: PLW1509
+    )
+    try:
+        # You might wonder why we're using `process.communicate` instead of `process.wait`.
+        # `process.wait` deadlocks if the process returns lots of data in a pipe. In our case,
+        # `process.wait` deadlocks when `rclone` returns a long directory listing through stdout.
+        stdout_str, stderr_str = process.communicate(timeout=timeout)
+    except (KeyboardInterrupt, SystemExit):
+        _terminate_child_process(process, process_name)
+        raise
+
+    _log_rclone_stderr(stderr_str)
+
+    if process.returncode == 0:
+        return stdout_str
+    else:
+        raise subprocess.CalledProcessError(
+            cmd=cmd, returncode=process.returncode, output=stdout_str, stderr=stderr_str
+        )
+
+
+def _log_rclone_stderr(stderr: str) -> None:
+    for line in stderr.splitlines():
+        clean_line = line.strip()
+        if not clean_line:
+            continue
+
+        # rclone uses "Error", "Warning", "Info" in its output.
+        line_upper = clean_line.upper()
+        if "ERROR" in line_upper or "FAILED" in line_upper:
+            log_level = logging.ERROR
+        elif "WARNING" in line_upper:
+            log_level = logging.WARNING
+        else:
+            log_level = logging.INFO
+
+        log.log(level=log_level, msg=f"Rclone: {clean_line}")
+
+
+def _set_death_signal() -> None:
+    """Send SIGTERM to the child process if the parent process (python) dies."""
+    libc = ctypes.CDLL("libc.so.6")  # Load the libc library to access prctl
+    pr_set_pdeathsig = 1
+    libc.prctl(pr_set_pdeathsig, signal.SIGTERM)
+
+
+def _terminate_child_process(process: subprocess.Popen, process_name: str) -> None:
+    log.exception("Terminating child process %s...", process_name)
+    process.terminate()
+    log.info(
+        "Waiting 5 seconds for child %s process to terminate gracefully...",
+        process_name,
+    )
+    try:
+        stdout_str, stderr_str = process.communicate(timeout=5)
+    except subprocess.TimeoutExpired:
+        log.warning(
+            "Child process %s has not terminated gracefully. So we will forcefully kill it.",
+            process_name,
+        )
+        process.kill()
+    else:
+        _log_rclone_stderr(stderr_str)
+        if stdout_str:
+            log.info("%s stdout during shutdown: %s", process_name, stdout_str)
