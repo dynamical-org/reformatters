643e1f35320eaff46058a5c3cc131b0e853ce593
Jack Kelly <jack@openclimatefix.org>
2026-01-31 16:09:07 +0000

    Handle missing directory. And other improvements.

--- src/reformatters/dwd/copy_files_from_dwd_with_copyurls.py
+++ src/reformatters/dwd/copy_files_from_dwd_with_copyurls.py
@@ -4,7 +4,7 @@ import threading
 from collections.abc import Sequence
 from datetime import datetime
 from pathlib import Path, PurePosixPath
-from subprocess import PIPE, CompletedProcess
+from subprocess import PIPE, CalledProcessError
 from typing import IO, Final
 
 from reformatters.common.logging import get_logger
@@ -34,8 +34,12 @@ def list_files(
             above 32, and this actually slows down file listing.
             For more info, see the rclone docs: https://rclone.org/docs/#checkers-int
         rclone_args: Additional args to be passed to `rclone lsf`.
+
+    Returns:
+        paths: A sorted list of all the files found in `path`. Returns an empty list if the
+        directory does not exist.
     """
-    log.info("Listing files on %s", path)
+    log.info("Listing files on '%s'...", path)
     checkers = checkers or 32
     cmd = (
         "rclone",
@@ -47,10 +51,27 @@ def list_files(
         f"--checkers={checkers:d}",
         *rclone_args,
     )
-    result = run_command(cmd)
-    paths = sorted(PurePosixPath(p) for p in result.stdout.splitlines())
-    log.info(f"Found {len(paths):,d} files on '{path}'.")
-    return paths
+    log.info("Running command: '%s'", " ".join(cmd))
+    try:
+        # TODO(Jack): Set timeout & env vars.
+        result = subprocess.run(cmd, check=True, text=True, capture_output=True)  # noqa: S603
+    except CalledProcessError as e:
+        if (
+            e.returncode == 3
+            and isinstance(e.stderr, str)
+            and "directory not found" in e.stderr.lower()
+        ):
+            log.info("Directory not found: '%s'", path)
+            return []
+        else:
+            log_error_from_called_process_error(e)
+            raise
+    else:
+        if result.stderr:
+            log.info("rclone stderr: %s", result.stderr)
+        paths = sorted(PurePosixPath(p) for p in result.stdout.splitlines())
+        log.info(f"Found {len(paths):,d} files on '{path}'.")
+        return paths
 
 
 def list_grib_files_on_dwd_https(
@@ -66,99 +87,128 @@ def list_grib_files_on_dwd_https(
     return list_files(path=path, checkers=checkers, rclone_args=rclone_args)
 
 
-def extract_nwp_var_from_dwd_path(dwd_path: PurePosixPath) -> str:
+def extract_nwp_var_from_src_path(src_path: PurePosixPath) -> str:
     """Check number of parts in the path & extract NWP variable name.
 
     Args:
-        dwd_path: A path on the DWD HTTPs server in the form:
+        src_path: A path on the DWD HTTPs server in the form:
             alb_rad/icon-eu_europe_regular-lat-lon_single-level_2026013003_000_ALB_RAD.grib2.bz2
     """
     n_expected_parts: Final[int] = 2
-    if len(dwd_path.parts) != n_expected_parts:
+    if len(src_path.parts) != n_expected_parts:
         raise RuntimeError(
             "Expected %d parts in the DWD path, found %d parts. Path: %s",
             n_expected_parts,
-            len(dwd_path.parts),
-            dwd_path,
+            len(src_path.parts),
+            src_path,
         )
-    return dwd_path.parts[0]
+    return src_path.parts[0]
 
 
 class DateExtractionError(Exception):
     pass
 
 
-def extract_nwp_init_datetime_from_dwd_path(
-    dwd_path_without_root: PurePosixPath,
+def extract_nwp_init_datetime_from_grib_filename(
+    grib_filename: str,
 ) -> datetime:
     dwd_nwp_init_date_regex: Final[re.Pattern[str]] = re.compile(r"_(\d{10})_")
-    nwp_init_date_match = dwd_nwp_init_date_regex.findall(dwd_path_without_root.name)
-    if len(nwp_init_date_match) == 0:
-        raise DateExtractionError("No date found in file: %s", dwd_path_without_root)
-    elif len(nwp_init_date_match) > 1:
+    nwp_init_date_matches = dwd_nwp_init_date_regex.findall(grib_filename)
+    if len(nwp_init_date_matches) == 0:
+        raise DateExtractionError("No date found in file: %s", grib_filename)
+    elif len(nwp_init_date_matches) > 1:
         raise DateExtractionError(
             "Expected exactly one 10-digit number in the filename (the NWP init date"
             " represented as YYYYMMDDHH), but instead found %d 10-digit numbers in path %s",
-            len(nwp_init_date_match),
-            dwd_path_without_root,
+            len(nwp_init_date_matches),
+            grib_filename,
         )
-    nwp_init_date_str = nwp_init_date_match[0]
+    nwp_init_date_str = nwp_init_date_matches[0]
     return datetime.strptime(nwp_init_date_str, "%Y%m%d%H")
 
 
-def format_datetime_for_use_in_dst_path(nwp_init_datetime: datetime) -> str:
+def format_datetime_for_dst_path(nwp_init_datetime: datetime) -> str:
     return nwp_init_datetime.strftime("%Y-%m-%dT%H")
 
 
-def convert_dwd_path_to_dst_path(dwd_path: PurePosixPath) -> PurePosixPath:
+def convert_src_path_to_dst_path(
+    src_path_starting_with_nwp_var: PurePosixPath,
+) -> PurePosixPath:
     """
     Args:
-        dwd_path: Should have exactly 2 parts: NWP_var/filename.grib2.bz2.
+        src_path_starting_with_nwp_var: Should have exactly 2 parts: NWP_var/filename.grib2.bz2.
     Returns:
         dst_path: In the form NWP_init_datetime/NWP_var/filename.grib2.bz2.
     """
-    nwp_init_datetime = extract_nwp_init_datetime_from_dwd_path(dwd_path)
-    nwp_init_datetime_str = format_datetime_for_use_in_dst_path(nwp_init_datetime)
-    nwp_var = extract_nwp_var_from_dwd_path(dwd_path)
-    dst_path = PurePosixPath(nwp_init_datetime_str) / nwp_var / dwd_path.name
+    grib_filename = src_path_starting_with_nwp_var.name
+    nwp_init_datetime = extract_nwp_init_datetime_from_grib_filename(grib_filename)
+    nwp_init_datetime_str = format_datetime_for_dst_path(nwp_init_datetime)
+    nwp_variable_name = extract_nwp_var_from_src_path(src_path_starting_with_nwp_var)
+    dst_path = PurePosixPath(nwp_init_datetime_str) / nwp_variable_name / grib_filename
     return dst_path
 
 
-def run_command(cmd: Sequence[str], log_stdout: bool = False) -> CompletedProcess[str]:
-    log.info("Running command: %s", " ".join(cmd))
-    result = subprocess.run(cmd, check=True, text=True, capture_output=True)
-    if result.stderr:
-        log.info("rclone stderr: %s", result.stderr)
-    if log_stdout and result.stdout:
-        log.info("rclone stdout: %s", result.stdout)
-    return result
+def log_error_from_called_process_error(e: CalledProcessError) -> None:
+    log.exception(
+        "stderr: %s; stdout: %s",
+        convert_called_process_error_output_to_str(e.stderr),
+        convert_called_process_error_output_to_str(e.stdout),
+    )
+
+
+def convert_called_process_error_output_to_str(output: None | str | bytes) -> str:
+    if output is None:
+        return ""
+    elif isinstance(output, str):
+        return output
+    else:
+        return output.decode()
 
 
 def run_command_with_concurrent_logging(cmd: Sequence[str]) -> int:
-    log.info("Running command: %s", " ".join(cmd))
-    process = subprocess.Popen(cmd, text=True, stdout=PIPE, stderr=PIPE, bufsize=1)
+    cmd_str = " ".join(cmd)
+    log.info("Running command: %s", cmd_str)
+    # TODO(Jack): Set timeout & env vars & catch KeboardException & terminate process.
 
-    # Create threads to read stdout and stderr simultaneously
-    t1 = threading.Thread(target=stream_reader, args=(process.stdout, "stdout"))
-    t2 = threading.Thread(target=stream_reader, args=(process.stderr, "stderr"))
+    try:
+        process = subprocess.Popen(cmd, text=True, stdout=PIPE, stderr=PIPE, bufsize=1)  # noqa: S603
 
-    t1.start()
-    t2.start()
+        # Create threads to read stdout and stderr simultaneously
+        t1 = threading.Thread(target=log_stdout, args=(process.stdout,))
+        t2 = threading.Thread(target=log_stderr_json, args=(process.stderr,))
 
-    # Wait for threads to finish (which happens when process closes the pipes)
-    t1.join()
-    t2.join()
+        t1.start()
+        t2.start()
 
-    return_code = process.wait()
-    log.info("return code = %s", return_code)
-    return return_code
+        # Wait for threads to finish (which happens when process closes the pipes)
+        t1.join()
+        t2.join()
+
+        return_code = process.wait()
+    except CalledProcessError as e:
+        log_error_from_called_process_error(e)
+        raise
+    except KeyboardInterrupt:
+        # Avoid having a zombie rclone process if user kills Python with Ctrl-C
+        log.warning("Received KeyboardInterrupt... terminating subprocess...")
+        process.terminate()
+        raise
+    else:
+        log.info("return code = %d after running command: '%s'", return_code, cmd_str)
+        return return_code
 
 
-def stream_reader(pipe: IO[str], prefix: str) -> None:
+def log_stdout(pipe: IO[str]) -> None:
     """Reads a pipe line-by-line and logs it."""
     with pipe:
         for line in pipe:
-            log.info(f"{prefix}: {line.strip()}")
+            log.info(f"stdout: {line.strip()}")
+
+
+def log_stderr_json(pipe: IO[str]) -> None:
+    with pipe:
+        for line in pipe:
+            log.info(f"stderr: {line.strip()}")
 
 
 def run_rclone_copyurls(csv_of_files_to_transfer: str) -> None:
@@ -178,6 +228,7 @@ def run_rclone_copyurls(csv_of_files_to_transfer: str) -> None:
         # Logging:
         "--stats=2s",  # Output statistics every 2 seconds.
         "--use-json-log",  # Output all logs in JSON.
+        # TODO(Jack): Delete these commented-out args if we no longer need them.
         # "--progress",
         # "--quiet",
         # "--stats-one-line",
@@ -190,9 +241,9 @@ def run_rclone_copyurls(csv_of_files_to_transfer: str) -> None:
 
 
 def list_files_on_dst_for_nwp_runs_available_from_dwd(
-    dwd_paths_without_root: Sequence[PurePosixPath],
-    dwd_root_path: PurePosixPath,
-    dst_root_path: PurePosixPath,
+    src_paths_starting_with_nwp_var: Sequence[PurePosixPath],
+    src_root_path_ending_with_init_hour: PurePosixPath,
+    dst_root_path_without_init_dt: PurePosixPath,
 ) -> list[PurePosixPath]:
     """The returned paths include (and start with) the NWP init datetime."""
     # Find unique NWP runs available from DWD. Usually, a DWD path like
@@ -201,20 +252,21 @@ def list_files_on_dst_for_nwp_runs_available_from_dwd(
     # be in the process of overwriting the files for yesterday's midnight run with today's midnight
     # run, and the 00/ directory will contain files from two NWP runs.
     unique_nwp_init_datetimes = {
-        extract_nwp_init_datetime_from_dwd_path(dwd_path_without_root)
-        for dwd_path_without_root in dwd_paths_without_root
+        extract_nwp_init_datetime_from_grib_filename(src_path.name)
+        for src_path in src_paths_starting_with_nwp_var
     }
     log.info(
-        f"Found {len(unique_nwp_init_datetimes)} unique NWP init datetime(s) in {dwd_root_path}: {unique_nwp_init_datetimes}"
+        f"Found {len(unique_nwp_init_datetimes)} unique NWP init datetime(s)"
+        f" in '{src_root_path_ending_with_init_hour}': {unique_nwp_init_datetimes}"
     )
 
     # Get a list of all the files in the destination:
     # The paths in this list start with and *include* the NWP init datetime part of the path.
     existing_dst_paths_starting_with_init_dt: list[PurePosixPath] = []
     for nwp_init_dt in unique_nwp_init_datetimes:
-        nwp_init_dt_str = format_datetime_for_use_in_dst_path(nwp_init_dt)
+        nwp_init_dt_str = format_datetime_for_dst_path(nwp_init_dt)
         dst_paths_starting_with_nwp_var = list_files(
-            str(dst_root_path / nwp_init_dt_str)
+            str(dst_root_path_without_init_dt / nwp_init_dt_str)
         )
         existing_dst_paths_starting_with_init_dt.extend(
             nwp_init_dt_str / dst_path for dst_path in dst_paths_starting_with_nwp_var
@@ -224,18 +276,17 @@ def list_files_on_dst_for_nwp_runs_available_from_dwd(
 
 
 def main() -> None:
-    dwd_root_path = PurePosixPath("/weather/nwp/icon-eu/grib/03/")
+    src_root_path = PurePosixPath("/weather/nwp/icon-eu/grib/03/")
     dst_root_path = PurePosixPath("/home/jack/data/ICON-EU/grib/rclone_copyurls/")
 
-    # These dwd_paths do not include the dwd_root_path.
-    dwd_paths_without_root = list_grib_files_on_dwd_https(
-        path=f"dwd-http:{dwd_root_path}",  # rclone_remote:path
+    src_paths_starting_with_nwp_var = list_grib_files_on_dwd_https(
+        path=f"dwd-http:{src_root_path}",  # rclone_remote:path
     )
 
     files_already_on_dst = list_files_on_dst_for_nwp_runs_available_from_dwd(
-        dwd_paths_without_root=dwd_paths_without_root,
-        dwd_root_path=dwd_root_path,
-        dst_root_path=dst_root_path,
+        src_paths_starting_with_nwp_var=src_paths_starting_with_nwp_var,
+        src_root_path_ending_with_init_hour=src_root_path,
+        dst_root_path_without_init_dt=dst_root_path,
     )
 
     # Prepare a CSV with two columns:
@@ -243,13 +294,13 @@ def main() -> None:
     # 2. The destination path, from the NWP init datetime onwards.
     # This is the format required by `rclone copyurls`.
     csv_of_files_to_transfer: list[str] = []  # Each list item is one line of the CSV.
-    for dwd_path in dwd_paths_without_root:
-        dst_path = convert_dwd_path_to_dst_path(dwd_path)
+    for src_path in src_paths_starting_with_nwp_var:
+        dst_path = convert_src_path_to_dst_path(src_path)
         if dst_path not in files_already_on_dst:
-            src_path = f"https://opendata.dwd.de{dwd_root_path / dwd_path}"
-            dst_path = convert_dwd_path_to_dst_path(dwd_path)
-            csv_of_files_to_transfer.append(f"{src_path},{dst_path}")
-    log.info("Planning to transfer %d files.", len(csv_of_files_to_transfer))
+            full_src_path = f"https://opendata.dwd.de{src_root_path / src_path}"
+            dst_path = convert_src_path_to_dst_path(src_path)
+            csv_of_files_to_transfer.append(f"{full_src_path},{dst_path}")
+    log.info(f"Planning to transfer {len(csv_of_files_to_transfer):,d} files.")
 
     run_rclone_copyurls("\n".join(csv_of_files_to_transfer))
 
