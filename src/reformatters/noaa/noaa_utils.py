dabfac24e7e59cc7043967b84997cf49ae7a48af
Alden Keefe Sampson <alden@dynamical.org>
2026-02-25 15:03:10 -0500

    NOMADS 110/req minute

--- src/reformatters/noaa/noaa_utils.py
+++ src/reformatters/noaa/noaa_utils.py
@@ -2,7 +2,8 @@ from reformatters.common.config_models import DataVar, INTERNAL_ATTRS_co
 from reformatters.common.download import RateLimiter
 
 # Documented NOMADS rate limit is 120 requests/minute
-nomads_rate_limiter = RateLimiter(max_per_minute=150)
+# When you get rate limited it's hard to get out, so set a little under.
+nomads_rate_limiter = RateLimiter(max_per_minute=110)
 
 # Retry on server errors, rate limits, and redirects (Akamai bot mitigation returns 302)
 NOMADS_RETRY_STATUS_CODES = {302, 429, 500, 502, 503, 504}
