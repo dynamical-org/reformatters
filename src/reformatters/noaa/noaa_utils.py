b13c085a1c0f153ba45a2d33f58b4d85ad930573
Alden Keefe Sampson <alden@dynamical.org>
2026-02-26 14:52:56 -0500

    Mark all HRRR precipitation related variables as missing hour 0 values (#466)
    There are values for categorical and % freezing precip in HRRR f000 files
    but they are all zeros.
    
    This change will cause HRRR analysis to pull the f001 values for the correct
    valid times so the HRRR analysis dataset gets useful categorical and % forzen
    precipitation values. It will also leave nans rather than 0 in HRRR forecast
    lead time = 0

--- src/reformatters/noaa/noaa_utils.py
+++ src/reformatters/noaa/noaa_utils.py
@@ -10,4 +10,6 @@ NOMADS_RETRY_STATUS_CODES = {302, 429, 500, 502, 503, 504}
 
 
 def has_hour_0_values(data_var: DataVar[INTERNAL_ATTRS_co]) -> bool:
+    if data_var.internal_attrs.hour_0_values_override is not None:
+        return data_var.internal_attrs.hour_0_values_override
     return data_var.attrs.step_type == "instant"
