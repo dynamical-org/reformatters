3eea2bb0b30caed1a3ca00be9f6eb91cdd6b222a
Alden Keefe Sampson <alden@dynamical.org>
2026-09-22 09:35:07 -0400

    Unsuspend the GEFS 16 day 0.5 degree virtual cron jobs after the manifest re-split (#1105)
    The store's manifests were re-split to the sizes set in #1100 (commit
    7Z53PAB1C30MVR1DK8EG, verified against the previous head), so the update and
    validate cron jobs resume.

--- src/reformatters/noaa/gefs/forecast_16_day_0_5_degree_virtual/dynamical_dataset.py
+++ src/reformatters/noaa/gefs/forecast_16_day_0_5_degree_virtual/dynamical_dataset.py
@@ -63,7 +63,6 @@ class NoaaGefsForecast16Day05DegreeVirtualDataset(
             secret_names=self.store_factory.k8s_secret_names(),
             workers_total=1,
             parallelism=1,
-            suspend=True,
         )
         validation_cron_job = ValidationCronJob(
             name=f"{cron_job_name_prefix}-validate",
@@ -75,7 +74,6 @@ class NoaaGefsForecast16Day05DegreeVirtualDataset(
             cpu="1.5",
             memory="7G",
             secret_names=self.store_factory.k8s_secret_names(),
-            suspend=True,
         )
 
         return [operational_update_cron_job, validation_cron_job]
